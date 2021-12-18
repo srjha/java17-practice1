9abdd6d95d4d03c5e0ac7d63abdd8221f0abb7fd
“Shashi <Shashi2587@gmail.com>
2021-12-18 22:41:32 +0100

    Fresh out of start spring io

---
+++ src/test/java/com/sr/pract/app/Java17Practice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.pract.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17Practice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
